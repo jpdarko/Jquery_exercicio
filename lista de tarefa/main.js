914fbdd22fa6d58f4b9908e33029bf3add44d687
João Paulo da Silva <130411286+jpdarko@users.noreply.github.com>
2023-08-09 14:08:12 -0300

    git push origin execicio_Jquery

---
+++ lista de tarefa/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $("#task-form").submit(function(event) {
+        event.preventDefault();
+        
+        const taskName = $("#task-name").val();
+        if (taskName.trim() !== "") {
+            const listItem = $("<li>").text(taskName);
+            listItem.click(function() {
+                $(this).toggleClass("completed");
+            });
+            $("#task-list").append(listItem);
+            $("#task-name").val("");
+        }
+    });
+});
